028bd29eaf862a85b543c104d1027542d3dd0e05
patrykferenc <patrykferenc@protonmail.com>
2023-07-05 20:59:04 +0200

    Remove autosave; Adjust gruvbox style.

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,7 +4,7 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  { import = "astrocommunity.editing-support.auto-save-nvim" },
+  -- { import = "astrocommunity.editing-support.auto-save-nvim" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.go" },
@@ -12,8 +12,10 @@ return {
   { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.svelte" },
   { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.typescript-all-in-one" },
-  { import = "astrocommunity.colorscheme.gruvbox" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.colorscheme.gruvbox-nvim" },
   { import = "astrocommunity.completion.copilot-lua-cmp" },
   {
     "gruvbox.nvim",
